94ea2e1bdc5111c3ac4bc4df37c0b64c33cd1d53
lorenzocosta <lorenzocosta@mbp-di-lorenzo>
2020-03-17 18:46:16 +0100

    Cambiato titolo finestra

--- src/main/java/it/polito/tdp/libretto/EntryPoint.java
+++ src/main/java/it/polito/tdp/libretto/EntryPoint.java
@@ -16,7 +16,7 @@ public class EntryPoint extends Application {
         Scene scene = new Scene(root);
         scene.getStylesheets().add("/styles/Styles.css");
         
-        stage.setTitle("JavaFX and Maven");
+        stage.setTitle("Sviluppato da Lorenzo Costa");
         stage.setScene(scene);
         stage.show();
     }
